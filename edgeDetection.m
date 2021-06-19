c75ab73d4815187392e1fe1795c84d4be6494931
8BitJonny <timon99@gmx.de>
2021-06-19 15:53:49 +0200

    :sparkles: subwindow aggregation v1

--- edgeDetection.m
+++ edgeDetection.m
@@ -1,6 +1,6 @@
 function [resultImg] = edgeDetection (img, ruleSet, neighborHood, subwindows)
 	% BASED ON SUBWINDOW/NEIGHBORHOOD SIZE -> BUILD PADDING AROUND IMAGE
-	biggestCoordOffset = abs(max(max(max(subwindows))))
+	biggestCoordOffset = abs(max(max(max(subwindows))));
 	img = extendWithBoundaryCondition(img, biggestCoordOffset);
 
 	iterations = 1;
@@ -10,54 +10,35 @@ function [resultImg] = edgeDetection (img, ruleSet, neighborHood, subwindows)
 	subWindowState = zeros(size(img));
 	figure(1);
 
-	% multiDimF = @(x, f) f(f(x));
-	% subWindowX = subwindows(1,1,:)
-	% subWindowY = subwindows(1,2,:)
-	% subWMinX = multiDimF (subWindowX, @min);
-	% subWMaxX = multiDimF (subWindowX, @max);
-	% subWMinY = multiDimF (subWindowY, @min);
-	% subWMaxY = multiDimF (subWindowY, @max);
-	% subwindowNeighborHood = zeros (subWMaxX - subWMinX + 1, subWMaxY - subWMinY + 1);
+	stringLookUp = "0123456789";
+	toString = @(num) stringLookUp(num+1);
 
-	function re = getSubwindowResult (x,y,world,subwindow)
-  		totalCellsInSubwindow = length(subwindow);
-  		aliveCells = countNeighborHood(x,y,world,subwindow);
-  		if (aliveCells / totalCellsInSubwindow > 0.5)
-  			re = 1;
-		elseif (aliveCells / totalCellsInSubwindow == 0.5)
-			re = round(rand(1));
-		else
-			re = 0;
-		endif
-	endfunction
-
+	countOfSubwindowElements = rows(subwindows);
+	countAlive = @(matrix) sum(matrix > 0);
+	calcSubwindowResults = @(img, subWindowIndexes, countOfSubwindowElements) round( countAlive(img(subWindowIndexes)) / countOfSubwindowElements );
 
 	for iter = 1:iterations
 		for ix = biggestCoordOffset+1:(nx - biggestCoordOffset)
 			for iy = biggestCoordOffset+1:(ny - biggestCoordOffset)
 				neighborHoodIndexes = calcIndexesFromCoords(ix, iy, ny, neighborHood);
 				subWindowIndexes = calcIndexesFromCoords(ix, iy, ny, subwindows);
-				subWindowState(:,:,:) = 0;
 
-				subWindowState(subWindowIndexes(1,:,:)) = round( sum(img(subWindowIndexes) > 0) / rows(subwindows) );
-				% Compute subwindows
-				% for index = 1:length(subwindows)
-    %     			subwindow = subwindows{index};
-    %     			subwindowNeighborHood(subwindow{1}(1)+2:subwindow{1}(2)+2) = getSubwindowResult(ix, iy, img, subwindow);
-				% endfor
-				% Cache subwindows
+				subWindowState(subWindowIndexes(1,:,:)) = calcSubwindowResults(img, subWindowIndexes, countOfSubwindowElements);
 
-				nlive = sum(subWindowState(neighborHoodIndexes) > 0);
-				% nlive = countNeighborHood(2,2,subwindowNeighborHood,neighborHood);
-				newState(ix, iy) = getfield(ruleSet, [mat2str(nlive) "-" mat2str(img(ix, iy))]);
+				nlive = countAlive(subWindowState(neighborHoodIndexes));
+
+				newState(ix, iy) = getfield(ruleSet, [toString(nlive) "-" toString(img(ix, iy))]);
+
+				% Reset SubWindowState
+				subWindowState(subWindowIndexes(1,:,:)) = 0;
 			end % loop over columns
 		end % loop over rows
 
 		img = newState;
 		
 		% Comment out to not show intermediate results
-		imshow(img);
-		pause(0.2)
+		% imshow(img);
+		% pause(0.2)
 
 	end % loop over iterations
 	resultImg = img;
--- utils/countNeighborHood.m
+++
@@ -1,8 +0,0 @@
-function aliveNeighbors = countNeighborHood (x,y,world,n_h)
-	aliveNeighbors = 0;
-	for index = 1:length(n_h)
-        neighbor = n_h{index};
-		neighborPixel = getPixel(x + neighbor(1), y + neighbor(2), world);
-		aliveNeighbors = aliveNeighbors + neighborPixel;
-	endfor
-endfunction
