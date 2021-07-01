228866d0c5dcf1d6b0ab045842a00df115066fb8
8BitJonny <timon99@gmx.de>
2021-07-01 17:17:45 +0200

    :bug: remove hardcoded neighborhood size

--- utils/calculateNeighborHoodIndexes.m
+++ utils/calculateNeighborHoodIndexes.m
@@ -1,5 +1,5 @@
 % (X x Y x NeighborHood Size) Matrix with 3D for NeighborHood Indices for each e
 function neighborHoodIndexes = calculateNeighborHoodIndexes (neighborHood, indexOffsetMatrix, padded_img_width, padding)
 	baseNeighborHoodIndexes = calculateIndexesFromCoords(padding, padded_img_width, neighborHood);
-	neighborHoodIndexes = indexOffsetMatrix + reshape(baseNeighborHoodIndexes,1,1,8);
+	neighborHoodIndexes = indexOffsetMatrix + reshape(baseNeighborHoodIndexes,1,1,size(neighborHood,1));
 endfunction
