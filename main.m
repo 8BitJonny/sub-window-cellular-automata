6db701a85aa4835a2a40504eba5421cd10896f02
8BitJonny <timon99@gmx.de>
2021-07-17 15:00:55 +0200

    :sparkles: input multiple images

--- main.m
+++ main.m
@@ -2,7 +2,10 @@
 %%%% SETUP
 pkg load image
 addpath(genpath("./utils"));
+set(0, "defaultfigurecolor", [0.3 0.3 0.3]);
 clf;
+progress_bar = waitbar(0);
+FAST = 0;
 
 % Neighborhoods need to be defined from center outwards in a clockwise spiral
 load "./NeighborHoods/Moore.mat" neighbor_hood
@@ -10,31 +13,81 @@ load "./ruleLookUpTable/MooreWithCenter.mat" pattern_lookup_matrix
 [gT_simple_rule, otca_832_rule] = loadRules({"C/Ground_Truth_Simple", "C/OTCA832"});
 [rosin_sub_window, no_effect_sub_window] = loadSubwindows({"PLRosin", "NoEffect"});
 
-imgs = loadImages({"blinker", "lena-binary", "apple-1", "bat-1", "beetle-6", "butterfly-20", "smiley"});
-img = imgs.("bat-1"); % Image to be used
+imgs = loadImages({"apple-1", "bat-1", "beetle-6", "butterfly-20", "elephant-1"});
+imgs = {imgs.("apple-1"), imgs.("bat-1")};
 
 %%%%%%%%%%%%%%%%%
 %%%% CALCULATIONS
-gT_without_noise = edgeDetection(img, gT_simple_rule, neighbor_hood, no_effect_sub_window);
-img = imnoise(img, "salt & pepper", 0.10);
+results = {};
+for i = 1:length(imgs)
+	img = imgs{i};
 
-plain_otca_result = edgeDetection(img, otca_832_rule, neighbor_hood, no_effect_sub_window);
-sub_window_otca_result = edgeDetection(img, otca_832_rule, neighbor_hood, rosin_sub_window);
+	% Calculate Ground Truth	
+	gT_without_noise = edgeDetection(img, gT_simple_rule, neighbor_hood, no_effect_sub_window);
 
-denoised_img = mat2gray(im2bw (imsmooth(img, "P&M")));
-cannyResult = edge(denoised_img, "Canny");
-sobelResult = edge(denoised_img, "Sobel");
-prewittResult = edge(denoised_img, "Prewitt");
+	% Apply Noise
+	img = imnoise(img, "salt & pepper", 0.15);
+	% img = double(imnoise(img, "speckle", 0.5) > 0.5);
+
+	% Apply base method
+	plain_otca_result = edgeDetection(img, otca_832_rule, neighbor_hood, no_effect_sub_window);
+	% Apply proposed method
+	sub_window_otca_result = edgeDetection(img, otca_832_rule, neighbor_hood, rosin_sub_window);
+	% Denoise for State of the art methods
+	denoised_img = mat2gray(im2bw (imsmooth(img, "P&M")));
+	% Apply state of the art methods
+	cannyResult = edge(denoised_img, "Canny");
+	sobelResult = edge(denoised_img, "Sobel");
+	prewittResult = edge(denoised_img, "Prewitt");
+
+	% Save Results
+	results(i) = struct(
+		"Input", img,
+		"Ground Truth", gT_without_noise,
+		"Plain OTCA832", plain_otca_result,
+		"Subwindow OTCA832", sub_window_otca_result,
+		"Canny", cannyResult,
+		"Sobel", sobelResult,
+		"Prewitt", prewittResult
+	);
+	waitbar (i / length(imgs), progress_bar, "Edge Detection");
+endfor
+
+algorithms = fieldnames(results{1})(!cellfun(@(x) (strcmp(x, "Input") || strcmp(x, "Ground Truth")), fieldnames(results{1})));
+
+%%%%%%%%%%%%%%
+%%%% PERF CALC
+[pratts_FoM root_mean_square_errors peak_signal_to_noise_ratio] = deal(struct());
+for i = 1:length(results)
+	ground_truth = results{i}.("Ground Truth");
+	for algo_i = 1:numel(algorithms)
+		key = algorithms{algo_i};
+		value = results{i}.(key);
+		if (i == 1)
+			[pratts_FoM.(key) root_mean_square_errors.(key) peak_signal_to_noise_ratio.(key)] = deal(zeros(0));
+		endif
+		root_mean_square_errors.(key)(end+1) = rmse(value, ground_truth);
+		peak_signal_to_noise_ratio.(key)(end+1) = psnr(value, ground_truth);
+		if (!FAST)
+			pratts_FoM.(key)(end+1) = prattsFigureOfMerit(ground_truth, value);
+		end
+	end
+	waitbar (i / length(results), progress_bar, "Calc Performance")
+endfor
+
+mean_performances = structfun(
+	@(y) structfun( @(x) mean(x), y, "UniformOutput", false),
+	struct(
+		"PFoM", pratts_FoM,
+		"RMSE", root_mean_square_errors,
+		"PSNR", peak_signal_to_noise_ratio
+	), "UniformOutput", false
+)
+
+if (FAST)
+	mean_performances = rmfield(mean_performances, 'PFoM')
+end
 
 %%%%%%%%%%%%%%%%%%%%
 %%%% RESULT PRINTING
-plots_to_print = struct();
-plots_to_print.("Input") = img;
-plots_to_print.("Ground Truth") = gT_without_noise;
-plots_to_print.("Plain OTCA832") = plain_otca_result;
-plots_to_print.("Subwindow OTCA832") = sub_window_otca_result;
-plots_to_print.("Canny") = cannyResult;
-plots_to_print.("Sobel") = sobelResult;
-plots_to_print.("Prewitt") = prewittResult;
-
-plotResults(plots_to_print, 0);
+plotResults(results{2}, mean_performances);
--- utils/debug/plotResults.m
+++ utils/debug/plotResults.m
@@ -1,12 +1,11 @@
-function result = plotResults(plots_to_print, fast = false)
+function result = plotResults(plots_to_print, performances_to_print)
 	clf;
 	f = figure(1);
-	set(f, "color", [0.3 0.3 0.3]);
 	set(0, "defaulttextcolor", "white");
 	set(0, "defaultaxesycolor", "white");
 	set(0, "defaultaxesxcolor", "white");
 
-	performances_to_print_count = ternary(fast, 2, 3);
+	performances_to_print_count = numfields(performances_to_print);
 	plots_to_print_count = numfields(plots_to_print);
 	plots_per_row = 2;
 	plot_i = 0;
@@ -20,30 +19,10 @@ function result = plotResults(plots_to_print, fast = false)
 		title(key);
 	end
 
-	% Calculate Performance
-	[pratts_FoM root_mean_square_errors peak_signal_to_noise_ratio] = deal(zeros(0));
-	ground_truth = plots_to_print.("Ground Truth");
-	for [value, key] = plots_to_print
-		if (!strcmp(key, "Input") && !strcmp(key, "Ground Truth"))
-			root_mean_square_errors(end+1) = rmse(value, ground_truth);
-			peak_signal_to_noise_ratio(end+1) = psnr(value, ground_truth);
-			if (!fast)
-				pratts_FoM(end+1) = prattsFigureOfMerit(ground_truth, value);
-			end
-		endif
-	end
-
-	performances_to_print = struct(
-		"RMSE", root_mean_square_errors,
-		"PSNR", peak_signal_to_noise_ratio,
-		"PFoM", pratts_FoM
-	);
-
 	% Plot performance Graph
 	for [value, key] = performances_to_print
-		if (fast && strcmp(key, "PFoM"))
-			continue
-		endif
+		labels = fieldnames(value);
+		value = [struct2cell(value){:}];
 		sorting_direction = ternary(strcmp(key, "RMSE"), "descend", "ascend");
 		[sorted_performance, sorted_indexes] = sort(value, sorting_direction);
 
@@ -51,6 +30,6 @@ function result = plotResults(plots_to_print, fast = false)
 		subplot (plot_rows, plot_cols, plot_i);
 		barh(sorted_performance);
 		title(key);
-		set(gca, "yTickLabel", fieldnames(plots_to_print)(3:end)(sorted_indexes,:))
+		set(gca, "yTickLabel", labels(sorted_indexes,:))
 	end
 endfunction
