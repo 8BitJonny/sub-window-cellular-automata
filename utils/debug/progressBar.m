448de5ac03ec58222263c3a48075a1dd076a1526
8BitJonny <timon99@gmx.de>
2021-07-20 21:50:58 +0200

    :bug: add check if old progressBar window still active

--- utils/debug/progressBar.m
+++ utils/debug/progressBar.m
@@ -1,7 +1,7 @@
 function result = progressBar(number, message)
 	persistent bar;
 	persistent size;
-	if (isempty (bar))
+	if (isempty (bar) || !ishandle(bar))
 		size = number*2;
     	bar = waitbar(0, message, "createcancelbtn", "setappdata (gcbf,'interrupt',true)");
 	else
